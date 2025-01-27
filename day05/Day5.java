e678ae2a6929c0de259e27e9bcfc86e05aeba560
realWallawalla <timon.sarakinis@gmail.com>
2025-01-27 09:07:58 +0100

    day5 start

--- day05/Day5.java
+++ day05/Day5.java
@@ -1,30 +1,73 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Day5 {
     public static void main(String[] args) throws IOException {
-        final List<char[]> strings;
-        try(var lines = Files.lines(Path.of("day05/input.txt"))) {
-            strings = lines.map(String::toCharArray).toList();
+        final List<String> rules = new ArrayList<>();
+        final List<String> updates = new ArrayList<>();
+
+        try(var lineStream = Files.lines(Path.of("day05/input.txt"))) {
+            List<String> lines = lineStream.toList();
+            int breakIndex = 0;
+            for (int i = 0; i < lines.size(); i++) {
+                if (lines.get(i).isEmpty()) {
+                    breakIndex = i;
+                    break;
+                } else {
+                    rules.add(lines.get(i));
+                }
+            }
+            for (int i = breakIndex + 1; i < lines.size(); i++) {
+                updates.add(lines.get(i));
+            }
         } catch (IOException e) {
             System.out.println("could not read input file");
             return;
         }
 
-        String part = System.getenv("part") == null ? "part2" : System.getenv("part");
+        String part = System.getenv("part") == null ? "part" : System.getenv("part");
         System.out.println(
-                part.equals("part2") ? new Day5().getPart2(strings) : new Day5().getPart1(strings));
+                part.equals("part2") ? new Day5().getPart2(rules, updates) : new Day5().getPart1(rules, updates));
     }
 
-    private int getPart1(List<char[]> input) {
+    private int getPart1(List<String> rules, List<String> updates) {
+        Map<String, Set<Integer>> sortedRules = new HashMap<>();
+        AtomicInteger sumOfMiddleNbr = new AtomicInteger();
+        AtomicBoolean updateOk = new AtomicBoolean(false);
+        for (String s : rules)
+         {
+            String[] split = s.split("\\|");
+            sortedRules.computeIfAbsent(split[0], _k -> new HashSet<>()).add(Integer.valueOf(split[1]));
+         };
+
+        updates.forEach(update -> {
+            String[] pages = update.split(",");
+            for (int i = 0; i< pages.length; i++) {
+                String page = pages[i];
+                Set<Integer> afterPages = sortedRules.get(page);
+                if (afterPages == null) {
+                   continue;
+                }
+                for (int j = i + 1; j < pages.length - 1; j++) {
+                    updateOk.set(afterPages.contains(Integer.valueOf(pages[j])));
+                    if (!updateOk.get()) {
+                          }
+                }
+
+            }
+            if (updateOk.get()) {
+                int midIndex = pages.length/2;
+                sumOfMiddleNbr.addAndGet(Integer.parseInt(pages[midIndex]));
+            }
+        });
+        return sumOfMiddleNbr.get();
+    }
+
+    private int getPart2(List<String> rules, List<String> updates) {
         return 0;
     }
-
-    private int getPart2(List<char[]> input) {
-        return 0;
-    }
-
-
 }
